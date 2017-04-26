43b70dce0cf9619f64cd4dbc209446122e35c9b5
Becca Saines <becca@streak.com>
2017-04-26 11:54:43 -0700

    my window closes on the click of my button

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 'use strict';
 
-
 console.log("IN INDEX.JS")
 const {ipcRenderer} = require('electron');
 var closeEl = document.querySelector('#close-button');
 
 closeEl.addEventListener('click', function () {
-    console.log("DUDE");
-}); 
-
-
-
-
-
-
+    window.close();
+});
