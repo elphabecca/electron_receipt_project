173c953c5b155b5aba5f991a8a4eb4fe0c68b107
Becca Saines <becca@streak.com>
2017-04-27 12:43:21 -0700

    HENRY IS THE BEST and now video works

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 console.log("IN INDEX.JS");
 const {ipcRenderer} = require('electron');
-// console.log(ipcRenderer.sendSync('synchronous-message', 'ping SYNC')); // prints "pong" from main.js, sends "ping SYNC" 
+// console.log(ipcRenderer.sendSync('synchronous-message', 'ping SYNC')); // prints "pong" from main.js, sends "ping SYNC"
 
 ipcRenderer.on('asynchronous-reply', (event, arg) => {
   console.log(arg, "in asynchronous-reply", new Date()); // prints "pong"
@@ -15,3 +15,24 @@ var closeEl = document.querySelector('#close-button');
 closeEl.addEventListener('click', function () {
     window.close();
 });
+
+var constraints = {
+  audio: false,
+video: {
+  width: {
+    ideal: 400
+  },
+  height: {
+    ideal: 200
+  }
+}
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+var video = document.querySelector('video')
+// Play stream in <video> element
+video.srcObject = stream
+}).catch(function (error) {
+console.error(error)
+})
